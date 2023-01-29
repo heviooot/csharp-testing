467eb380d169df74ad9023031d36e4c3d06bd75a
Harold Ulaen <ulaen.haroldoj@gmail.com>
2023-01-29 15:49:53 +0800

    first commit

---
+++ TestingTable/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TestingTable.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
---
+++ TestingTable/Models/Sob.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace TestingTable.Models
+{
+	public class Sob
+    {
+        public IEnumerable<SobData> data { get; set; }
+    }
+    public class SobData
+    {
+        public object policy_no { get; set; }
+        public object pol_end_no { get; set; }
+        public DateTime post_date { get; set; }
+        public object distribution_code { get; set; }
+        public object distribution_name { get; set; }
+        public string distribution_type { get; set; }
+        public object acting_code { get; set; }
+        public object lob_code { get; set; }
+        public object pol_office { get; set; }
+        public double premium { get; set; }
+        public int comm { get; set; }
+        public int disc { get; set; }
+    }
+
+
+}
+
